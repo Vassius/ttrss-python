13a076c735562913b122cbcd4a9282a7d4c834e9
Markus Wiik <vassius@gmail.com>
2013-03-28 22:39:16 +0100

    Fix bug in TTRAuth

--- ttrss/auth.py
+++ ttrss/auth.py
@@ -14,17 +14,25 @@ class TTRAuth(AuthBase):
         if int(j['status']) == 0:
             return r
 
-        s = requests.Session()
-        res = s.post(r.request.url, json.dumps({
+        sid = None
+        if r.headers['set-cookie']:
+            sid = r.headers['set-cookie'].split(';')[0].split('=')[1]
+            r.request.headers['Cookie'] = 'ttrss_api_sid={0}'.format(sid)
+        else:
+            sid = r.request.headers['Cookie'].split('=')[1]
+
+        res = requests.post(r.request.url, json.dumps({
+            'sid': sid,
             'op': 'login',
             'user': self.user,
             'password': self.password
         }))
         raise_on_error(res)
 
-        _r = s.post(r.request.url, r.request.body)
+        r.request.deregister_hook('response', self.response_hook)
+        _r = requests.Session().send(r.request)
+        _r.cookies = r.cookies
         raise_on_error(_r)
-        _r.headers.update(res.headers)
 
         return _r
 
