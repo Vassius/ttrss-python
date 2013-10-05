4f6e61844139a22fd860b9e8151cc95b3b8a5a35
Markus Wiik <vassius@gmail.com>
2013-10-05 23:23:49 +0200

    Bump version to 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
         name='ttrss-python',
-        version='0.3',
+        version='0.4',
         description='A client library for the Tiny Tiny RSS web API',
         long_description=long_desc,
         url='https://github.com/Vassius/ttrss-python',
